64dcbdbe8605344f6ad839196734ef9b2bac51e3
//de <code@extremist.digital>
2015-09-01 11:52:07 -0700

    add code of conduct, rspec params, and move shared examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 require "rubygems"
 require "bundler/setup"
+
+require 'celluloid/current'
+require 'celluloid/io'
+
+Dir["./spec/support/*.rb", "./spec/shared/*.rb"].map { |f| require f }
+
+RSpec.configure do |config|
+  config.filter_run focus: true unless ENV["CI"]
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+  config.profile_examples = 3
+end
